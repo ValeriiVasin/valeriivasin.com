8d306b6201747aa289e051288055ccdcb9b4471b
Valerii Vasin <valerii.vasin@gmail.com>
2024-10-25 20:22:52 +0000

    feat: tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+const config = /** @type {Config} */ ({
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
+export default config;
